579f589120a6914c72a98c734e0ef7f6a5adf8f6
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2026-06-21 21:58:57 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "✅ ";
-} catch (PDOException $e) {
+} catch (PDOException $e) { 
     die("❌.. Database connection failed: " . $e->getMessage());
 }
     
